c38035efd4b007ed43ed21681ef13f6ae6ecd2d2
shub <choubeys331@gmail.com>
2024-05-02 11:49:34 +0530

    my first airbnb demo project

---
+++ src/main/java/com/flight/reservation/model/Flight.java
@@ -0,0 +1,11 @@
+package com.flight.reservation.model;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Flight {
+private int id;
+private String name;
+private String destination;
+
+}
---
+++ src/test/java/com/flight/reservation/FlightReservationSystem1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flight.reservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightReservationSystem1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
